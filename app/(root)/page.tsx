04bf92f44f190867c7f52f8d3f484a163407a90e
NIPUN0BUGS <nipunsanjulasamarakoon@gmail.com>
2024-07-30 18:29:18 +0530

    implement layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,37 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSideBar from '@/components/RightSideBar'
+import TotalBalance from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName:'Nipun', lastName:'Smrkn', email:'nipun@example.com' } 
+
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+           <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user={loggedIn?.firstName || 'guest'}
+            subtext='Access and manage your account and transactions efficiently.'
+           />
+           <TotalBalance
+           accounts={[]}
+           totalBanks={1}
+           totalCurrentBalance={1000.30}
+           />
+        </header>
+        {/* Add your main content here */}
+      </div>
+      <RightSideBar
+       user={loggedIn}
+       transactions={[]}
+       banks={[{currentBalance: 123.50},{
+        currentBalance: 500.50}]}
+       />
+    </section>
   )
 }
 
